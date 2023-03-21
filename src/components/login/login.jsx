02435d6597eaef02b35c26e28cac0909169e5d8f
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-03-21 21:13:46 +0000

    modified

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,54 +1,77 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import "../styles/form.scss";
+import Alert from "../alert/Alert";
 
 export default function login() {
-	const [email, setEmail]= useState('');
-	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState({});
 
-	const handleSubmit =(e)=>{
-    e.preventDefault()
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-	 const user ={
-		email,
-		password
-	 };
-     console.log(user)
+		const user = {
+			identifier: email,
+			password: password,
+		};
+		//  console.log(user)
+		// Using Axios to speak to backend
+		try {
+			const response = await axios.post(
+				"http://localhost:1337/api/auth/local",
+				user,
+			);
+			//clear form fields
+			setEmail("");
+			setPassword("");
+			setError({})
+			console.log(response);
+
+		} 
+		catch (err) 
+		
+		{
+			setError(err.response.data.error);
+		}
 	};
 
 	return (
-		<form className="form form--page" onSubmit={handleSubmit}>
-			<div className="form__group form__group--page">
-				<label className="form__label">Email</label>
-				<input
-					className="form__field"
-					type="text"
-					placeholder="Email"
-					value={email}
-					onChange={(e)=> setEmail(e.target.value)}
-		
-				/>
-			</div>
+		<>
+			{error.message && <Alert type='error' error={error} />}
+			<form className="form form--page" onSubmit={handleSubmit}>
+				<div className="form__group form__group--page">
+					<label className="form__label">Email</label>
+					<input
+						className="form__field"
+						type="text"
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
 
-			<div className="form__group form__group--page">
-				<label className="form__label">Password</label>
-				<input
-					className="form__field"
-					type="password"
-					placeholder="Password"
-					value={password}
-					onChange={(e)=> setPassword(e.target.value)}
-				/>
-			</div>
+				<div className="form__group form__group--page">
+					<label className="form__label">Password</label>
+					<input
+						className="form__field"
+						type="password"
+						placeholder="Password"
+						autoComplete="off"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
 
-			<div className="form__group form__group--page">
-				<input className="form__btn" type="submit" value="Login" />
-			</div>
+				<div className="form__group form__group--page">
+					<input className="form__btn" type="submit" value="Login" />
+				</div>
 
-			<footer>
-				Dont have an account? <Link to="/register">Register</Link>
-			</footer>
-		</form>
+				<footer>
+					Dont have an account? <Link to="/register">Register</Link>
+				</footer>
+			</form>
+		</>
 	);
 }
